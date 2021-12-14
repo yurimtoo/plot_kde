2c3f6c83d954c537fa1e09418e858ee446b8d7b7
yurimtoo <96141247+yurimtoo@users.noreply.github.com>
2021-12-14 13:40:40 -0500

    Updated plotting

--- plot.py
+++ plot.py
@@ -20,9 +20,8 @@ for i, line in enumerate(lines[1:]):
 #nshares = nshares[nshares<1000] # this ~triples the probability
 # For clarity, if you use either of the above trimmings, then the probability density is a measure of the probability that an ape with under 2500 (or 1000) shares is holding X shares
 
-# Plot the histogram -- 250 bins is sufficient for this, 
-# higher numbers just add noise to the result
-nbins = 250
+# Plot the histogram
+nbins = 1000
 ncount, bins, patches = plt.hist(nshares, bins=nbins, 
                                  density=True, log=True, 
                                  label='Histogram')
@@ -33,14 +32,22 @@ plt.savefig('histogram.png')
 
 # KDE to determine the underlying dist'n
 rep = ss.gaussian_kde(nshares)
-val = rep.evaluate(bins)
+val = rep.evaluate(bins) # Evaluate the KDE according to the histogram bins
 
 plt.plot(bins, val, label='KDE')
 plt.legend(loc='best')
 plt.savefig('histogram_with_kde_log.png')
+# Trim to under 1000 shares for prettier plots
+plt.xlim(-10, 1000)
+plt.ylim(1e-5, 1e-1)
+plt.savefig('histogram_with_kde_log_under1k.png')
 plt.yscale('linear')
+plt.ylim(0, 0.02) # so that the plot looks reasonable...
+plt.savefig('histogram_with_kde_linear_under1k.png')
 plt.ylim(0, 0.007)
+plt.xlim(-850, 17500) # back to orignial scale
 plt.savefig('histogram_with_kde_linear.png')
 plt.close()
+
 
 
