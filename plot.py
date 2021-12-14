dea0598245ec97f74c44e14cc75f0b313523088d
yurimtoo <96141247+yurimtoo@users.noreply.github.com>
2021-12-14 13:18:35 -0500

    Initial commit

---
+++ plot.py
@@ -0,0 +1,46 @@
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+
+plt.ion()
+
+# Read the summary file
+with open('summary.dat', 'r') as foo:
+    lines = foo.readlines()
+
+# Extract out number of shares
+nshares = np.zeros(len(lines)-1, dtype=int)
+for i, line in enumerate(lines[1:]):
+    nshares[i] = int(line.split(';')[-1])
+
+# Uncomment this if you want to omit the giant whales
+#nshares = nshares[nshares<2500] # this ~doubles the probability
+# Uncomment this if you want to omit all whales w/ > 1000 shares
+#nshares = nshares[nshares<1000] # this ~triples the probability
+# For clarity, if you use either of the above trimmings, then the probability density is a measure of the probability that an ape with under 2500 (or 1000) shares is holding X shares
+
+# Plot the histogram -- 250 bins is sufficient for this, 
+# higher numbers just add noise to the result
+nbins = 250
+ncount, bins, patches = plt.hist(nshares, bins=nbins, 
+                                 density=True, log=True, 
+                                 label='Histogram')
+plt.xlabel('Number of shares')
+plt.ylabel('Probability density')
+plt.title('Distribution of $GME Shareholders')
+plt.savefig('histogram.png')
+
+# KDE to determine the underlying dist'n
+rep = ss.gaussian_kde(nshares)
+val = rep.evaluate(bins)
+
+plt.plot(bins, val, label='KDE')
+plt.legend(loc='best')
+plt.savefig('histogram_with_kde_log.png')
+plt.yscale('linear')
+plt.ylim(0, 0.007)
+plt.savefig('histogram_with_kde_linear.png')
+plt.close()
+
+
